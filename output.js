137b12659460c438e3246bf1eba6647a8aaa8526
cssvamsi <cssvamsi@gmail.com>
2017-04-20 19:24:50 -0500

    Assignment

---
+++ output.js
@@ -0,0 +1,9 @@
+var sum = require('./ar');
+var mul = require('./ar');
+var sub = require('./ar');
+var div = require('./ar');
+
+console.log(sum.add(111,122));
+console.log(sum.mul(11,22));
+console.log(sum.sub(16,18));
+console.log(sum.div(36,18));
